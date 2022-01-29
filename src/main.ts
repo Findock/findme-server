5cafb2a2e45200fe8a04d4e387fc907526fc9c9d
Mateusz Ożóg <mateusz2345678@gmail.com>
2022-01-29 10:15:50 +0100

    project init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { version } from '../package.json';
+import figlet = require('figlet');
+import gradient = require('gradient-string');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  figlet('FindMe Server v' + version, (err, data) => {
+    console.log(gradient.pastel.multiline(data));
+    console.log(gradient.pastel(' © 2022 Findock'));
+  });
 }
 bootstrap();
