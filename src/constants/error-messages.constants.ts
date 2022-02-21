a717cbe0b860410f85b466d95758b73a016af0f5
Mateusz Ożóg <mateusz2345678@gmail.com>
2022-02-21 23:58:36 +0100

    Fixes typo

--- src/constants/error-messages.constants.ts
+++ src/constants/error-messages.constants.ts
@@ -1,5 +1,5 @@
 export default {
     USER_WITH_THIS_EMAIL_ALREADY_EXIST: 'User with this email already exists.',
-    USER_WITH_THIS_EMAIL_DOES_NOT_EXIST: 'User with this email does not exists.',
+    USER_WITH_THIS_EMAIL_DOES_NOT_EXIST: 'User with this email does not exist.',
     WRONG_PASSWORD: 'Wrong password.',
 };
--- src/dto/error-exception.dto.ts
+++ src/dto/error-exception.dto.ts
@@ -4,7 +4,7 @@ export default class ErrorExceptionDto {
     @ApiProperty({ example: 401 })
     statusCode: number;
 
-    @ApiProperty({ example: [ 'User with this email does not exists.' ] })
+    @ApiProperty({ example: [ 'User with this email does not exist.' ] })
     message: string[];
 
     @ApiProperty({ example: 'Unauthorized' })
