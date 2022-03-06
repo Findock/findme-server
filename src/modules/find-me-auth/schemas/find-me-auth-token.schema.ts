7a9c8ca9c997e9b37feaf506104563e071926e39
Mateusz Ożóg <mateusz2345678@gmail.com>
2022-03-06 11:08:38 +0100

    Auth token schema

---
+++ src/modules/find-me-auth/schemas/find-me-auth-token.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+
+@Schema()
+export class FindMeAuthToken {
+    @ApiProperty()
+    @Prop({ required: true })
+    public deviceName: string;
+
+    @ApiProperty()
+    @Prop({ required: true })
+    public token: string;
+
+    @ApiProperty()
+    @Prop({ default: new Date() })
+    public lastUsed: Date;
+}
+
+export type FindMeAuthTokenDocument = FindMeAuthToken & Document;
+
+export const FindMeAuthTokenSchema = SchemaFactory.createForClass(FindMeAuthToken);
