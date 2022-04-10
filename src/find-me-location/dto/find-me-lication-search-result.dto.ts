f7651755eaf3e93a66b533f7efd456bbaabc1d2d
Mateusz Ożóg <mateusz2345678@gmail.com>
2022-04-10 15:01:32 +0200

    Swagger update

--- src/find-me-location/dto/find-me-lication-search-result.dto.ts
+++ src/find-me-location/dto/find-me-lication-search-result.dto.ts
@@ -1,33 +1,82 @@
 import { ApiProperty } from "@nestjs/swagger";
 
+export class NominatimAddressObject {
+    @ApiProperty()
+    public continent?: string;
+
+    @ApiProperty()
+    public country?: string;
+
+    @ApiProperty()
+    public country_code?: string;
+
+    @ApiProperty()
+    public region?: string;
+
+    @ApiProperty()
+    public state?: string;
+
+    @ApiProperty()
+    public state_district?: string;
+
+    @ApiProperty()
+    public county?: string;
+
+    @ApiProperty()
+    public "ISO3166-2-lvl"?: string;
+
+    @ApiProperty()
+    public municipality?: string;
+
+    @ApiProperty()
+    public city?: string;
+
+    @ApiProperty()
+    public town?: string;
+
+    @ApiProperty()
+    public village?: string;
+
+    @ApiProperty()
+    public city_district?: string;
+
+    @ApiProperty()
+    public district?: string;
+
+    @ApiProperty()
+    public borough?: string;
+
+    @ApiProperty()
+    public suburb?: string;
+
+    @ApiProperty()
+    public hamlet?: string;
+
+    @ApiProperty()
+    public croft?: string;
+
+    @ApiProperty()
+    public isolated_dwelling?: string;
+
+    @ApiProperty()
+    public neighbourhood?: string;
+
+    @ApiProperty()
+    public allotments?: string;
+
+    @ApiProperty()
+    public quarter?: string;
+
+    @ApiProperty()
+    public road?: string;
+
+    @ApiProperty()
+    public postcode?: string;
+}
+
 export class FindMeLocationSearchResultDto {
     @ApiProperty()
-    public address: {
-        continent?: string;
-        country?: string;
-        country_code?: string;
-        region?: string;
-        state?: string;
-        state_district?: string;
-        county?: string;
-        "ISO3166-2-lvl"?: string;
-        municipality?: string;
-        city?: string;
-        town?: string;
-        village?: string;
-        city_district?: string;
-        district?: string;
-        borough?: string;
-        suburb?: string;
-        hamlet?: string;
-        croft?: string;
-        isolated_dwelling?: string;
-        neighbourhood?: string;
-        allotments?: string;
-        quarter?: string;
-        road?: string;
-        postcode?: string;
-    };
+    public address: NominatimAddressObject;
 
     @ApiProperty()
     public lat: number;
