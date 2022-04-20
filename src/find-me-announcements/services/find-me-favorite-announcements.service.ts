37ff6a0fae776d2e3b3e87e9e09a561add508f5a
Mateusz Ożóg <mateusz2345678@gmail.com>
2022-04-20 23:44:42 +0200

    Service methods

--- src/find-me-announcements/services/find-me-favorite-announcements.service.ts
+++ src/find-me-announcements/services/find-me-favorite-announcements.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
+import { FindMeAnnouncement } from "@/find-me-announcements/entities/find-me-announcement.entity";
 import { FindMeFavoriteAnnouncement } from "@/find-me-announcements/entities/find-me-favorite-announcement.entity";
+import { FindMeUser } from "@/find-me-users/entities/find-me-user.entity";
 
 @Injectable()
 export class FindMeFavoriteAnnouncementsService {
@@ -10,4 +12,38 @@ export class FindMeFavoriteAnnouncementsService {
         @InjectRepository(FindMeFavoriteAnnouncement)
         private favoriteAnnouncementsRepository: Repository<FindMeFavoriteAnnouncement>
     ) { }
+
+    public async addAnnouncementToUserFavorites(
+        announcement: FindMeAnnouncement,
+        user: FindMeUser
+    ): Promise<FindMeFavoriteAnnouncement> {
+        const favoriteAnnouncement = this.favoriteAnnouncementsRepository.create({
+            announcement,
+            user,
+        });
+        await this.favoriteAnnouncementsRepository.save(favoriteAnnouncement);
+        return favoriteAnnouncement;
+    }
+
+    public async removeAnnouncementFromUserFavorites(
+        announcement: FindMeAnnouncement,
+        user: FindMeUser
+    ): Promise<void> {
+        const favoriteAnnouncement = await this.favoriteAnnouncementsRepository.findOne({
+            announcement,
+            user,
+        });
+        this.favoriteAnnouncementsRepository.remove(favoriteAnnouncement);
+    }
+
+    public async isAnnouncementInUserFavorites(
+        announcement: FindMeAnnouncement,
+        user: FindMeUser
+    ): Promise<boolean> {
+        const favoriteAnnouncement = await this.favoriteAnnouncementsRepository.findOne({
+            announcement,
+            user,
+        });
+        return !!favoriteAnnouncement;
+    }
 }
